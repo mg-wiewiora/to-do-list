87ac7c53399c61c263f36a7399a670e9d54c051f
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-11 02:04:13 +0100

    function block rearranged

--- js/script.js
+++ js/script.js
@@ -27,28 +27,32 @@
 
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
     const init = () => {
         render();
 
         const form = document.querySelector(".js-form");
 
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
-            if (newTaskContent === "") {
-                return;
-            };
-
-            tasks.push({
-                content: newTaskContent,
-            });
-
-            render();
-
-        });
-
+        form.addEventListener("submit", onFormSubmit)
     };
 
     init();
